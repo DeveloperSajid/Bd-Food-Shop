877e14d90635dc29de215dce8f3995aef5dfdee8
DeveloperSajid <mdsajidahmed08@gmail.com>
2024-07-04 20:48:52 +0600

    install bootstrap and add some files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  }
+}
